2b76ea5aa63a0901f96d46e9d46868bcc0866e70
Diego Alves <diegoteialves@gmail.com>
2026-08-12 22:31:43 -0300

    Exercicios de fixação de objetos

---
+++ 02-objetos/exer1.js
@@ -0,0 +1,30 @@
+// Crie um objeto chamado:
+// produto
+
+// Ele deve possuir:
+// nome
+// preco
+// estoque
+// categorias
+
+// categorias deve ser um array com pelo menos duas categorias.
+
+// Depois:
+// Mostre o nome no console.
+// Mostre a segunda categoria no console.
+// Altere o preço.
+// Adicione uma propriedade chamada disponivel.
+// Mostre o objeto inteiro no console.
+
+const produto = {
+    nome: "Diego Alves",
+    preco: 190,
+    estoque: 20,
+    categorias: ["Brinquedos", "Roupas", "Eletronicos"]
+}
+
+console.log(produto.nome);
+console.log(produto.categorias[1]);
+produto.preco = 200;
+produto.disponivel = true;
+console.log(produto);
