5541eae1fd78d8a784117292faca77aa8a0babb3
Diego Alves <diegoteialves@gmail.com>
2026-08-12 23:04:39 -0300

    Implementando metodos

--- 02-objetos/exer1.js
+++ 02-objetos/exer1.js
@@ -16,36 +16,72 @@
 // Adicione uma propriedade chamada disponivel.
 // Mostre o objeto inteiro no console.
 
+// const produto = {
+//     nome: "Diego Alves",
+//     preco: 190,
+//     estoque: 20,
+//     categorias: ["Brinquedos", "Roupas", "Eletronicos"]
+
+// }
+
+// console.log(produto.nome);
+// console.log(produto.categorias[1]);
+// produto.preco = 200;
+// produto.disponivel = true;
+// console.log(produto);
+
+// // Pegue o mesmo objeto produto e adicione uma propriedade chamada fabricante.
+
+// // Só que fabricante também será um objeto:
+
+// // fabricante
+// //    ├── nome
+// //    ├── pais
+// //    └── anoFundacao
+
+// // Depois faça o programa mostrar somente o país do fabricante.
+
+// produto.fabricante = {
+//     nome: "Nike",
+//     pais: "Brazil",
+//     anoFundacao: 2026
+// }
+
+// console.log(produto.fabricante.pais);
+// console.log(produto);
+
+// // No seu objeto produto, crie um método chamado:
+// // vender
+// // Quando executarmos:
+// // produto.vender();
+// // ele deve diminuir o estoque em 1.
+// // Por exemplo, se:
+// // estoque: 20
+// // depois de:
+// // produto.vender();
+// // deve ficar:
+// // estoque: 19
+// // E faça:
+// // produto.vender();
+// // produto.vender();
+// // produto.vender();
+// // No final, mostre o estoque no console. Ele deverá estar em 17.
+
 const produto = {
     nome: "Diego Alves",
     preco: 190,
     estoque: 20,
-    categorias: ["Brinquedos", "Roupas", "Eletronicos"]
+    categorias: ["Brinquedos", "Roupas", "Eletronicos"],
+
+    vender: function()  {
+        this.estoque--;
+    }
 }
 
-console.log(produto.nome);
-console.log(produto.categorias[1]);
-produto.preco = 200;
-produto.disponivel = true;
-console.log(produto);
+produto.vender();
+produto.vender();
+produto.vender();
 
-// Pegue o mesmo objeto produto e adicione uma propriedade chamada fabricante.
+console.log(produto.estoque);
 
-// Só que fabricante também será um objeto:
-
-// fabricante
-//    ├── nome
-//    ├── pais
-//    └── anoFundacao
-
-// Depois faça o programa mostrar somente o país do fabricante.
-
-produto.fabricante = {
-    nome: "Nike",
-    pais: "Brazil",
-    anoFundacao: 2026
-}
-
-console.log(produto.fabricante.pais);
-console.log(produto);
 
