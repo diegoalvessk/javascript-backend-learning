a71cbb3e9b8b3212202b6d91a4ad786563b805ee
diegoalvessk <diegoteialves@gmail.com>
2026-08-14 22:09:13 -0300

    Metodos depositar, sacar e consultarSaldo funcionado

--- 02-objetos/exer2.js
+++ 02-objetos/exer2.js
@@ -13,11 +13,29 @@ const contaBancaria = {
     },
 
     sacar: function(valor){
-         if(valor <= 0){
+        if(valor <= 0){
             console.log("Valor inválido");
             return;
         }
 
+        if(valor > this.saldo){
+            console.log("Saldo insuficiente");
+            return;
+        }
+        
+        this.saldo = this.saldo - valor;
+        console.log(`Saldo atual: ${this.saldo}`);
+    },
 
-    }
+    consultarSaldo: function(){
+        console.log(`Saldo atual: ${this.saldo}`);
+    } 
 }
+
+contaBancaria.consultarSaldo();
+contaBancaria.depositar(500);
+contaBancaria.sacar(200);
+contaBancaria.sacar(2000);
+contaBancaria.depositar(-100);
+contaBancaria.sacar(0);
+contaBancaria.consultarSaldo();
